afa1a9632dee10448263e15d9f2dfd7af0ab8c54
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-20 15:52:49 -0300

    Adicionando o segundo exercício
    Exercício sobre escrever na tela. "Um programa que apresenta uma etiqueta de endereço."

---
+++ A2-escrevendonatela.java
@@ -0,0 +1,11 @@
+class Teste{
+    public static void main (String[] args){
+        
+        System.out.printf("Nome completo: Gabriele de Carvalho\nEndereço: Rua das Areias, Tapera, Florianópolis\nCEP: 88049-300   Telefone: (48) 99832-0115\n");
+        
+        // Escrito do jeito anterior pode ficar complicado de ver o código, pode ser escrito assim:
+        // System.out.println("Nome completo: Gabriele de Carvalho");
+        // System.out.println("Endereço: Rua das Areias, Tapera, Florianópolis");
+        // System.out.println("CEP: 88049-300   Telefone: (48) 99832-0115");
+    }
+}
