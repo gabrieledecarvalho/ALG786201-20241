91e07edd49a568e73c05e05ea6b77e8c4e9ff69f
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-20 16:12:26 -0300

    Update and rename the file to U2A01E03escrevenatela.java
    Modificando o código dos exercícios para UXAYYEZZ
    U: unidade
    A: aula
    E: exercício

--- U2A01E03escrevenatela.java
+++ U2A01E03escrevenatela.java
@@ -1,4 +1,12 @@
-class Teste{
+/* U2A01E01 - Um programa que desenha um quadrado
+Escreva um programa que mostre a seguinte figura no alto da tela:
+XXXXX
+X   X
+X   X
+X   X
+XXXXX
+*/
+class U2A01E03escrevenatela{
     public static void main (String[] args){
         //desenhando o desenho pedido no exercício:
         System.out.println("XXXX");
