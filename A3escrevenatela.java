73b67d04031b97b2ce0e982bc50e8cc33b7b0d01
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-20 16:09:02 -0300

    Adicionando A3
    Um programa que desenha um quadrado.

---
+++ A3escrevenatela.java
@@ -0,0 +1,17 @@
+class Teste{
+    public static void main (String[] args){
+        //desenhando o desenho pedido no exercício:
+        System.out.println("XXXX");
+        System.out.println("X  X");
+        System.out.println("X  X");
+        System.out.println("X  X");
+        System.out.println("XXXX\n");
+        //desenhando um quadrado:
+        System.out.println("X X X X");
+        System.out.println("X     X");
+        System.out.println("X     X");
+        System.out.println("X X X X\n");
+        
+        System.out.printf("X X X X\nX     X\nX     X\nX X X X\n");
+    }
+}
