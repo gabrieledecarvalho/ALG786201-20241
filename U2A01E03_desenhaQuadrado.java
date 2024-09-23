598dc5e2e4c27269a71caba2575d7583fcd8e75e
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-23 11:17:06 -0300

    Update and rename to U2A01E03_desenhaQuadrado.java
    Adicionando alguns comentários e renomeando o programa de U2A01E03escrevenatela.java para U2A01E03_desenhaQuadrado.java

--- U2A01E03_desenhaQuadrado.java
+++ U2A01E03_desenhaQuadrado.java
@@ -6,20 +6,22 @@ X   X
 X   X
 XXXXX
 */
-class U2A01E03escrevenatela{
+class U2A01E03_desenhaQuadrado{
     public static void main (String[] args){
-        //desenhando o desenho pedido no exercício:
+        // mostrando na tela o desenho pedido no exercício:
         System.out.println("XXXX");
         System.out.println("X  X");
         System.out.println("X  X");
         System.out.println("X  X");
         System.out.println("XXXX\n");
-        //desenhando um quadrado:
+        
+        // desenhando um quadrado:
         System.out.println("X X X X");
         System.out.println("X     X");
         System.out.println("X     X");
         System.out.println("X X X X\n");
         
-        System.out.printf("X X X X\nX     X\nX     X\nX X X X\n");
+        // outro modo de desenhar um quadrado
+        System.out.print("X X X X\nX     X\nX     X\nX X X X\n");
     }
 }
