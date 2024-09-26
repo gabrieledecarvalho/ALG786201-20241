feda53171f7834b8db549d99c7c6f1ac374818bf
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-26 15:40:13 -0300

    Update U2A01E01_escrevePoema.java
    Arrumando alguns parágrafos e comentários

--- U2/U2A01E01_escrevePoema.java
+++ U2/U2A01E01_escrevePoema.java
@@ -1,22 +1,22 @@
-/* U2A01E01 - Um programa que escreve um poema:
-Neste exercício, você deve escrever um programa que mostre algumas informações na tela. Ele deve apresentar este poema:
-O meu nome é Severino,
-não tenho outro de pia.
-Como há muitos Severinos,
-que é santo de romaria,
-deram então de me chamar
-Severino de Maria;
-como há muitos Severinos
-com mães chamadas Maria,
-fiquei sendo o da Maria
-do finado Zacarias.
-
-Cada linha do texto deve ser apresentada com uma instrução.  Ao final, seu programa deve mostrar uma linha em branco, e logo em seguida isto:
-Neste poema, a palavra Severino aparece XX vezes, e outras YY vezes apareceu Maria.
-... sendo que XX e YY são números inteiros que devem ser substituídos nessa frase final.
+/*  U2A01E01 - Um programa que escreve um poema:
+    Neste exercício, você deve escrever um programa que mostre algumas informações na tela. Ele deve apresentar este poema:
+    O meu nome é Severino,
+    não tenho outro de pia.
+    Como há muitos Severinos,
+    que é santo de romaria,
+    deram então de me chamar
+    Severino de Maria;
+    como há muitos Severinos
+    com mães chamadas Maria,
+    fiquei sendo o da Maria
+    do finado Zacarias.
+    
+    Cada linha do texto deve ser apresentada com uma instrução.  Ao final, seu programa deve mostrar uma linha em branco, e logo em seguida isto:
+    Neste poema, a palavra Severino aparece XX vezes, e outras YY vezes apareceu Maria.
+    ... sendo que XX e YY são números inteiros que devem ser substituídos nessa frase final.
 */
 
-// Solução E1:
+// Solução U2A01E1:
 class U2A01E01_escrevePoema { //a palavra que segue class deve ser a mesma do nome do programa
     public static void main(String[] args) {
         // utilizando print e com \n para separar as linhas
