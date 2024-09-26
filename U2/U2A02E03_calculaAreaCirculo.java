30195b872907978d437b3f733158996c73d614d7
Gabriele de Carvalho <carvalhodegabriele@gmail.com>
2024-09-26 16:04:31 -0300

    Adicionando o programa U2A02E03
    Programa que calcula área de um círculo, dado um R

---
+++ U2/U2A02E03_calculaAreaCirculo.java
@@ -0,0 +1,22 @@
+/*  U2A02E03 - Cálculo de área de um círculo
+    Escreva um programa que calcule a área de um círculo, a qual pode ser feita com esta fórmula:
+    A = pi*(r^2)
+    ... sendo r o raio do círculo, e π uma constante com valor aprox. 3.1416. O valor do raio deve ser informado via teclado.
+    Seu programa deve mostrar uma mensagem na tela pedindo o raio do círculo. 
+    Em seguida, ele deve mostrar a área calculada.
+*/
+
+//  Solução U2A02E03:
+import java.util.Scanner;
+
+public class U2A02E03_calculaAreaCirculo {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in); 
+        double a, r;
+        System.out.print("Digite o raio do círculo: ");
+        r = leitor.nextDouble();
+        final double pi = 3.1416; //declarando uma constante
+        a = pi*r*r;
+        System.out.printf("A área do círculo é: %.2f.", a);
+    }
+}
